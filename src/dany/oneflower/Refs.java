1d17672a85bf89269e90b4e785ef84adf7a0980c
CatDany <dany2001ru@gmail.com>
2014-08-24 14:46:47 +0700

    Hotfix

--- src/dany/oneflower/Refs.java
+++ src/dany/oneflower/Refs.java
@@ -4,7 +4,7 @@ import dany.oneflower.libs.Helper;
 
 public class Refs
 {
-	public static final String VERSION = "b1.0-1";
+	public static final String VERSION = "b1.0-2";
 	
 	public static final String[] AUTHORS = new String[]
 			{
